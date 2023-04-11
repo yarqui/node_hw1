d7e46aece130cb6695ebfdd4043d226cb4044e79
yarqui <y.pelykh@gmail.com>
2023-04-11 21:30:05 +0300

    ➕ add fs functions to contacts.js

--- contacts.js
+++ contacts.js
@@ -1,5 +1,60 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-console.log("contactsPath:", contactsPath);
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  return allContacts.find((item) => item.id === contactId) || null;
+};
+
+// TODO: REMOVE
+// (async () => {
+//   console.log(await getContactById("qdggE76Jtbfd9eWJHrssH"));
+// })();
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = allContacts.splice(index, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+  return result;
+};
+
+// TODO: REMOVE
+// (async () => {
+//   console.log(await removeContact("qdggE76Jtbfd9eWJHrssH"));
+// })();
+
+const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+  return newContact;
+};
+
+// TODO: REMOVE
+// addContact("Stan", "dogm@gmg.com", "32355434");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-console.log("1st try");
-console.log("2nd try");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-let books = [
-  { id: 1, title: "farm" },
-  { id: 2, title: "farm2" },
-  { id: 3, title: "farm3" },
-  { id: 4, title: "farm4" },
-  { id: 5, title: "farm5" },
-];
+// let books = [
+//   { id: 1, title: "farm" },
+//   { id: 2, title: "farm2" },
+//   { id: 3, title: "farm3" },
+//   { id: 4, title: "farm4" },
+//   { id: 5, title: "farm5" },
+// ];
 
-const deleteById = (id) => {
-  const index = books.findIndex((item) => item.id === id);
-  console.log("index:", index);
+// const deleteById = (id) => {
+//   const index = books.findIndex((item) => item.id === id);
+//   console.log("index:", index);
 
-  if (index === -1) {
-    return null;
-  }
-  const [result] = books.splice(index, 1);
-  console.log("books:", books);
-  console.log("result:", result);
-};
+//   if (index === -1) {
+//     return null;
+//   }
+//   const [result] = books.splice(index, 1);
+//   console.log("books:", books);
+//   console.log("result:", result);
+// };
 
-deleteById(4);
+// deleteById(4);
