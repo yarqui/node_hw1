0fb863944dafcee5b9fe074dec9253bc8c93fdb4
yarqui <108986486+yarqui@users.noreply.github.com>
2023-04-12 16:08:18 +0300

    ➕ add commander pckg, update index.js
    - add invokeAction function
    - add program

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Command } = require("commander");
 const {
   listContacts,
   getContactById,
@@ -5,24 +6,44 @@ const {
   addContact,
 } = require("./contacts");
 
-// let books = [
-//   { id: 1, title: "farm" },
-//   { id: 2, title: "farm2" },
-//   { id: 3, title: "farm3" },
-//   { id: 4, title: "farm4" },
-//   { id: 5, title: "farm5" },
-// ];
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// const deleteById = (id) => {
-//   const index = books.findIndex((item) => item.id === id);
-//   console.log("index:", index);
+program.parse(process.argv);
 
-//   if (index === -1) {
-//     return null;
-//   }
-//   const [result] = books.splice(index, 1);
-//   console.log("books:", books);
-//   console.log("result:", result);
-// };
+const argv = program.opts();
 
-// deleteById(4);
+// TODO:
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await addContact(
+        name,
+        email,
+        phone
+      ); /**Should I use {name, email, phone} ?*/
+      return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
